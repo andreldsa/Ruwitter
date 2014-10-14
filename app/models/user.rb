40d3f797a7ffaa0102d9c3fef7b28293b27c9660
Andre Luiz dos Santos Abrantes <andredossantosabrantes@gmail.com>
2014-10-14 21:06:33 +0100

    fix password digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,13 @@
 class User < ActiveRecord::Base
-  #Name validation
-  validates :name, length: {in: 4..30}
-  
-  #Email validation
+  validates :name, presence: true, length: { in: 9..30 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: false}
-  
-  #Password validation and security
-  validates :password, length: { minimum: 6 }
+  validates :email, presence: true,
+                        format: { with: VALID_EMAIL_REGEX },
+                        uniqueness: { case_sensitive: false }
+  before_save { |user| user.email = email.downcase }
+
+  validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
-  has_secure_password
   
-  #Processing before save
-  before_save { |user| user.email = email.downcase}
+  has_secure_password
 end
