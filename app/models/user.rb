4eefc43629b5068ab36ff6010fd504cb9b365af7
Andre Luiz dos Santos Abrantes <andredossantosabrantes@gmail.com>
2014-10-14 16:35:38 +0100

    fix User email validation error

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   
   #Email validation
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { width: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: false}
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: false}
   
   #Password validation and security
   validates :password, length: { minimum: 6 }
